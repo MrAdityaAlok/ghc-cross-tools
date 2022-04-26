7b03ab1b0afeb72a9b89de679effa961edefb789
Aditya Alok <dev.aditya.alok@gmail.com>
2022-04-26 14:14:04 +0530

    test

--- scripts/ghc-slave/build.sh
+++ scripts/ghc-slave/build.sh
@@ -6,50 +6,14 @@ TERMUX_PKG_VERSION=8.10.7
 TERMUX_PKG_SRCURL="https://downloads.haskell.org/~ghc/${TERMUX_PKG_VERSION}/ghc-${TERMUX_PKG_VERSION}-src.tar.xz"
 TERMUX_PKG_SHA256=e3eef6229ce9908dfe1ea41436befb0455fefb1932559e860ad4c606b0d03c9d
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_BUILD_DEPENDS="ghc-libs-static"
+TERMUX_PKG_BUILD_DEPENDS="ghc-libs-static, libiconv, libffi, libgmp, haskell-network"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--flags=+network"
 
-termux_step_configure() {
-	termux_setup_cabal
-	termux_setup_ghc_cross_compiler
-}
-
-termux_step_make() {
-	cd $TERMUX_PKG_SRCDIR/utils/remote-iserv
-	cp -r $TERMUX_PKG_SRCDIR/libraries/libiserv .
+termux_step_post_get_source() {
+	cp -r $TERMUX_PKG_SRCDIR/libraries/libiserv $TERMUX_PKG_SRCDIR/utils/remote-iserv
+	export TERMUX_PKG_SRCDIR="${TERMUX_PKG_SRCDIR}/utils/remote-iserv"
 	cat >cabal.project <<-EOF
 		packages: .
 		   libiserv/
 	EOF
-	cabal configure \
-		--enable-static \
-		--prefix=$TERMUX_PREFIX \
-		--configure-option=--disable-rpath \
-		--configure-option=--disable-rpath-hack \
-		--configure-option=--host=${TERMUX_HOST_PLATFORM} \
-		--configure-option=CFLAGS="$CFLAGS" \
-		--configure-option=LDFLAGS="$LDFLAGS" \
-		--configure-option=CPPFLAGS="$CPPFLAGS" \
-		--configure-option=CXXFLAGS="$CXXFLAGS" \
-		--ghc-option=-optl-Wl,-rpath=$TERMUX_PREFIX/lib \
-		--ghc-option=-optl-Wl,--enable-new-dtags \
-		--with-compiler="$(command -v termux-ghc)" \
-		--with-ghc-pkg="$(command -v termux-ghc-pkg)" \
-		--with-hsc2hs="$(command -v termux-hsc2hs)" \
-		--hsc2hs-option=--cross-compile \
-		--with-ld=$LD \
-		--with-strip=$STRIP \
-		--with-ar=$AR \
-		--with-pkg-config=$PKG_CONFIG \
-		--with-happy="$(command -v happy)" \
-		--with-alex="$(command -v alex)" \
-		--extra-include-dirs=$TERMUX_PREFIX/include \
-		--extra-lib-dirs=$TERMUX_PREFIX/lib \
-		--disable-tests \
-		--flags=+network \
-		--enable-executable-static
-	cabal build
-}
-termux_step_make_install() {
-	cd $TERMUX_PKG_SRCDIR/utils/remote-iserv
-	cabal install
 }
