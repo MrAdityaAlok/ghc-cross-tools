da273b71644fbf4da19d0e19d4ec40a9d209cac2
Aditya Alok <dev.aditya.alok@gmail.com>
2023-02-22 01:14:23 +0530

    script: Refacotor, add iserv-proxy
    Signed-off-by: Aditya Alok <dev.aditya.alok@gmail.com>

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,43 +1,101 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
-export TAR_OUTPUT_DIR
+set -e -u
+
 TAR_OUTPUT_DIR="$(realpath "$1")"
-
 ARCH="$2"
 RELEASE_TAG="$3"
+
+ROOT="$(pwd)"
+BINDIR="${ROOT}/bin"
+
+source ./utils.sh
+
+export PATH="${BINDIR}:${PATH}"
+mkdir -p "$TAR_OUTPUT_DIR"
+mkdir -p "${BINDIR}"
+
+build_cabal() {
+	setup_ghc
+	setup_cabal
+	local version=3.8.1.0
+	local srcurl="https://github.com/haskell/cabal/archive/Cabal-v${version}.tar.gz"
+	local sha256=d4eff9c1fcc5212360afac8d97da83b3aff79365490a449e9c47d3988c14b6bc
+
+	local tar_tmpfile && tar_tmpfile="$(mktemp -t cabal.XXXXXX)"
+	download "${srcurl}" "${tar_tmpfile}" "${sha256}"
+
+	local build_dir && build_dir="$(mktemp -d -t cabal-install.XXXXXX)"
+	tar -xf "${tar_tmpfile}" -C "${build_dir}" --strip-components=1
+
+	cd "${build_dir}"
+	(
+		cd ./Cabal
+		patch -p1 <"${ROOT}"/cabal-install/correct-host-triplet.patch
+	)
+
+	mkdir -p "${build_dir}/bin"
+	cabal install cabal-install \
+		--install-method=copy \
+		--installdir="${build_dir}/bin" \
+		--project-file=cabal.project.release \
+		--enable-library-stripping \
+		--enable-executable-stripping \
+		--enable-split-sections \
+		--enable-executable-static
+
+	tar -cJf "${TAR_OUTPUT_DIR}/cabal-install-${version}.tar.xz" -C "${build_dir}"/bin .
+}
+
+build_iserv_proxy() {
+	setup_ghc
+	setup_cabal
+
+	version=9.2.5
+
+	cd "$ROOT"/iserv-proxy
+	mkdir -p ./bin
+
+	cabal install iserv-proxy\
+		--project-file=cabal.project \
+		--install-method=copy \
+		--installdir="$(realpath ./bin)" \
+		-flibrary -fproxy --constraint="libiserv +network" \
+		--enable-executable-stripping \
+		--enable-executable-static
+
+	tar -cvJf "${TAR_OUTPUT_DIR}/iserv-proxy-${version}.tar.xz" -C ./bin .
+}
 
 PKG_NAME="${RELEASE_TAG/-v*/}"
 PKG_VERSION="${RELEASE_TAG/*-v/}"
 
-mkdir -p "${TAR_OUTPUT_DIR}"
+echo "Compiling: $PKG_NAME:$PKG_VERSION"
 
-echo "Compiling: ${PKG_NAME}:${PKG_VERSION}"
-
-clone_termux_packages() {
-	# clone termux-packages into container
-	tmp_dir="$(mktemp -d -t termux-packages-XXXXXXXXXX)"
-	git clone https://github.com/termux/termux-packages.git "$tmp_dir/termux-packages"
-
-	cd "$tmp_dir/termux-packages" && git checkout haskell-9.2.5
-	mv -f "$tmp_dir"/termux-packages/* /home/builder/termux-packages
-}
-
-if [ "${PKG_NAME}" = "ghc" ]; then
+if [ "$PKG_NAME" = "ghc" ]; then
 	clone_termux_packages
 
-	cd /home/builder/termux-packages || exit
+	cd termux-packages
 	mkdir -p ./packages/ghc-cross
 	cp ./packages/ghc-libs/*.patch ./packages/ghc-cross
 	cp -r ./ghc/* ./packages/ghc-cross
 
-	./build-package.sh -I -a "${ARCH}" ghc-cross
+	./build-package.sh -I -a "$ARCH" ghc-cross
 
 elif [ "$PKG_NAME" = "cabal-install" ]; then
 	# Cabal is for x86_64 so build only once.
-	[ "${ARCH}" != "aarch64" ] && {
+	if [ "$ARCH" != "aarch64" ]; then
 		touch "${TAR_OUTPUT_DIR}/.placeholder"
 		tar -cJf "${TAR_OUTPUT_DIR}/placeholder-archive.tar.gz" -C "${TAR_OUTPUT_DIR}" .placeholder
 		exit 0
-	}
-	bash ./build_cabal.sh "${PKG_NAME}"
+	fi
+	build_cabal
+elif [ "$PKG_NAME" = "iserv-proxy" ]; then
+	# Cabal is for x86_64 so build only once.
+	if [ "$ARCH" != "aarch64" ]; then
+		touch "${TAR_OUTPUT_DIR}/.placeholder"
+		tar -cJf "${TAR_OUTPUT_DIR}/placeholder-archive.tar.gz" -C "${TAR_OUTPUT_DIR}" .placeholder
+		exit 0
+	fi
+	build_iserv_proxy
 fi
